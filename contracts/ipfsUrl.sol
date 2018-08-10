765b1aa1ee567dccef3654de1c4d7e031384f602
daiki01240 <domo01240@gmail.com>
2018-08-10 22:49:44 +0900

    [add] string combined contract

---
+++ contracts/ipfsUrl.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.23;
+import "github.com/Arachnid/solidity-stringutils/strings.sol";
+
+contract IpfsFactory {
+    using strings for *;
+    string ipfsUrls = "https://ipfs.io/ipfs/";
+    uint id = 0;
+
+    mapping (uint => string) public ipfsToNumber;
+    
+        function ipfsSet(string ipfsHash) public {
+            ipfsToNumber[id] = ipfsHash;
+            id++;
+        }
+        
+        function ipfsGet(uint ipfsNum) public view returns (string){
+            return ipfsUrls.toSlice().concat(ipfsToNumber[ipfsNum].toSlice()); // "abcdef"ipfsUrls
+        }    
+}
